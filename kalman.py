43c5f1897bcb8bb1356ea1d9c2c9c96089aaa148
Wenchang Gao MAC <wenchang.gao@tufts.edu>
2023-02-13 10:44:52 -0500

    prediction to be verified

---
+++ kalman.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class KalmanFilter:
+    '''
+    Kalman Filter algorithm
+    '''
+
+    def __init__(self, A, B, R, C, Q):
+        assert isinstance(A, np.ndarray)
+        assert isinstance(B, np.ndarray)
+        assert isinstance(R, np.ndarray)
+        assert A.shape[0] == B.shape[0] and A.shape[0] == R.shape[0]
+
+        self.A = A
+        self.B = B
+        self.R = R
+
+        self.C = C
+        self.Q = Q
+
+    def predict(self, μ, Σ, u):
+        assert self.A.shape[1] == μ.shape[0]
+        assert self.B.shape[1] == u.shape[0]
+        μ_hat = np.dot(self.A, μ)
+        Σ_hat = np.dot(np.dot(self.A, Σ), self.A.T)+self.R
+
+        return μ_hat, Σ_hat
+
+    def measure(self):
+        pass
